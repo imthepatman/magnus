337c6fb46882badd71b688eca03eff452e597c77
imthepatman <patrickwilhelm96@gmail.com>
2019-12-20 15:09:37 +0100

    Generalization to arbitrary order. To be tested.

--- magnus.py
+++ magnus.py
@@ -4,27 +4,43 @@ from scipy.linalg import expm
 from scipy.special import bernoulli, factorial
 import itertools as it
 
+sig_x = np.array([[0., 1.], [1., 0.]])
+sig_y = np.array([[0., -1.j], [1.j, 0.]])
+sig_z = np.array([[1., 0.], [0., -1.]])
+
+def get_example(id='std'):
+    if id == 'std':
+        A = lambda t: np.array([[1j, 2 + 1j - np.cos(2 * np.pi * t)], [-2 + 1j + np.cos(2 * np.pi * t), 3 * 1j]])
+        y0 = np.array([1., 1.]) / np.sqrt(2.)
+    elif id == 'rect':
+        f = lambda t: 1. if t>=0. else 0.
+        A = lambda t: -1.j*f(t)*(sig_x*np.cos(t) - sig_y*np.sin(t))
+        y0 = np.array([1., 1.]) / np.sqrt(2.)
+        #y0 = np.array([1., 0.])
+
+    return A, y0
+
 
 class MagnusIntegrator:
-    def __init__(self, A, order=5, N_int=10, qf='midpoint'):
+    def __init__(self, A, order=2, qf='midpoint'):
         self.A = A
         self.order = order
-        self.N_int = N_int
         self.qf = qf
 
         self.b_n = bernoulli(self.order)
         self.fac_n = np.array([factorial(n) for n in range(self.order + 1)])
 
-        self.ks = [[self.gen_ks(n, j) for j in range(1, n)] for n in range(2, self.order + 1)]
-
-        #self.fOmega = self.gen_fOmega()
-        # quit()
+        self.ks = {}
+        for n in range(2, self.order + 1):
+            for j in range(1, n):
+                self.ks['{}_{}'.format(n, j)] = self.gen_ks(n, j)
 
     def Omega(self, t_s, t_e):
         self.t0 = t_s
-        Om = self.Om(t_e, n=1)
+        Om = self.fOmega(t_e, n=1)
         for n in range(2, self.order + 1):
-            Om += self.Om(t_e, n=n)
+            Om_n = self.fOmega(t_e, n=n)
+            Om += Om_n
 
         return Om
 
@@ -43,38 +59,29 @@ class MagnusIntegrator:
 
     ### Auxiliary functions ###
 
-    def comm(self, M1, M2):
-        return M1 @ M2 - M2 @ M1
-
-    def Om(self, s, n=1):
+    def fOmega(self, s, n=1):
         if n < 2:
             Om_1 = self.quad(self.A, s)
             return Om_1
         else:
-            Om_n = self.quad(self.Omega_p, s, n=n)
+            Om_n = self.quad(self.fOmega_p, s, n=n)
             return Om_n
 
-    def Omega_p(self, s, n=2):
+    def fOmega_p(self, s, n=2):
         omp = 0.
         for j in range(1, n):
-            print('j', j)
-
             k_js = self.gen_ks(n, j)
-
             S_j = 0.
-
             # loop over possible variations for given j
             for k in k_js:
                 # right sided, nested commutators
-
-                B_k = self.comm(self.Om(s, n=k[0]), self.A(s))
+                B_k = self.comm(self.fOmega(s, n=k[0]), self.A(s))
                 # loop through indices in variation
                 for ik in range(1, len(k)):
-                    B_k += self.comm(self.Om(s, n=k[ik]), B_k)
+                    B_k += self.comm(self.fOmega(s, n=k[ik]), B_k)
 
                 S_j += B_k
             # sum over j before integration
-
             omp += self.b_n[j] / self.fac_n[j] * S_j
         return omp
 
@@ -91,73 +98,22 @@ class MagnusIntegrator:
         k_js = k_var[mask]
         return k_js
 
-    def gen_fOmega(self, t):
-        fdict = {}
-        fdict['Om_1'] = lambda s: self.quad(self.A, s)
-
-        b_n = bernoulli(self.order)
-        fac_n = np.array([factorial(n) for n in range(self.order + 1)])
-
-        for n in range(2, self.order + 1):
-            fdict['C_{}_{}'.format(n, 0)] = lambda s: 0.
-            print('\nn', n)
-            for j in range(1, n):
-                print('j', j)
-
-                # generate possible variations of k which sum to n - 1
-                k_range = np.arange(1, n - j + 1)
-                k_var = np.array(list(it.product(k_range, repeat=j)))
-                if len(k_var) > 0:
-                    if j < 2:
-                        k_var = k_var.reshape((len(k_var), 1))
-                    k_sum = np.sum(k_var, axis=1)
-
-                    mask = k_sum == n - 1
-                k_js = k_var[mask]
-                print(k_js)
-
-                fdict['S_{}_{}_{}'.format(n, j, 0)] = lambda s: 0.
-
-                # loop over possible variations for given j
-                for jk, k in enumerate(k_js):
-                    # right sided, nested commutators
-
-                    fdict['B_{}_{}_{}_{}'.format(n, j, jk, 0)] = lambda s: self.comm(fdict['Om_{}'.format(k[0])](s), self.A(s))
-                    # loop through indices in variation
-                    for ik in range(1, len(k)):
-                        fdict['B_{}_{}_{}_{}'.format(n, j, jk, ik)] = lambda s: self.comm(fdict['Om_{}'.format(k[ik])](s), fdict['B_{}_{}_{}_{}'.format(n, j, jk, ik - 1)](s))
-
-                    fdict['S_{}_{}_{}'.format(n, j, jk + 1)] = lambda s: fdict['S_{}_{}_{}'.format(n, j, jk)](s) + fdict['B_{}_{}_{}_{}'.format(n, j, jk, len(k) - 1)](s)
-                # sum over j before integration
-
-                fdict['C_{}_{}'.format(n, j)] = lambda s: fdict['C_{}_{}'.format(n, j - 1)](s) + b_n[j] / fac_n[j] * fdict['S_{}_{}_{}'.format(n, j, len(k_js))](s)
-
-            fdict['Om_{}'.format(n)] = lambda s: self.quad(fdict['C_{}_{}'.format(n, n - 1)], s)
-
-        print(fdict.keys())
-
-        self.t0 = 0.
-        print(fdict['C_{}_{}'.format(2, 1)](0.))
-        quit()
-
-        return fdict
+    def comm(self, M1, M2):
+        return M1 @ M2 - M2 @ M1
 
     def quad(self, f, t, **kwargs):
-        t0 = kwargs.get('t0', self.t0)
-        if t0 in kwargs:
-            kwargs.pop('t0')
-
         if self.qf == 'midpoint':
-            dt = t - t0
-            res = f(t0 + dt / 2., **kwargs) * dt
+            dt = t - self.t0
+            res = f(self.t0 + dt / 2., **kwargs) * dt
 
             return res
         elif self.qf == 'simpson':
-            ts = np.linspace(t0, t, self.N_int, endpoint=True)
-            fs = np.array([f(ti) for ti in ts])
-            res = integrate.simps(fs, ts, axis=0)
+            #TODO to implement
 
-            return res
+            return None
 
-    def norm(self, y):
-        return np.sqrt(np.real(np.sum(np.conj(y) * y, axis=-1)))
+    def t_braket(self, phi, psi_t):
+        return np.einsum('i, ni->n', np.conj(phi), psi_t)
+
+def braket(phi, psi):
+    return np.sum(np.conj(phi) * psi, axis=-1)
--- main.py
+++ main.py
@@ -1,30 +1,34 @@
-from magnus import MagnusIntegrator
+from magnus import MagnusIntegrator, get_example, braket
 import numpy as np
 import matplotlib.pyplot as plt
 
-A = lambda t: np.array([[1j, 2 + 1j - np.cos(2 * np.pi * t)], [-2 + 1j + np.cos(2 * np.pi * t), 3 * 1j]])
 
-#A = lambda t: 1j*np.array([[1., 0.], [0., -1.]])
-y0 = np.array([1.,1.]) / np.sqrt(2.)
-#y0 = np.array([1., 0.])
-
-T = 1.
+T = 10.
 tau = 0.01
+id = 'rect'
+
+A, y0 = get_example(id)
 
 mi = MagnusIntegrator(A, order=4)
 
 ts, ys = mi.evolve(y0, T, tau)
 
+y_test = np.array([1., -1.]) / np.sqrt(2.)
+tp = np.abs(mi.t_braket(y_test, ys))
+
 fig = plt.figure(figsize=(9, 6))
 ax_1 = fig.add_subplot(211)
 ax_2 = fig.add_subplot(212)
 
-ax_1.plot(ts, np.real(ys[:, 0]), '-', ts, np.real(ys[:, 1]), '-')
-#ax_1.set_xlabel(r'$t$')
-ax_1.set_ylabel(r'$Re\{\Psi\}$')
+ax_1.plot(ts, np.real(ys[:, 0]), '--', label=r'$Re\{Psi_0\}$')
+ax_1.plot(ts, np.real(ys[:, 1]), ':', label=r'$Re\{Psi_1\}$')
 
-ax_2.plot(ts, mi.norm(ys))
+ax_1.plot(ts, tp, '-', label=r'$T$')
+#ax_1.set_xlabel(r'$t$')
+ax_1.legend()
+
+ax_2.plot(ts, np.real(braket(ys, ys)))
 ax_2.set_xlabel(r'$t$')
-ax_2.set_ylabel(r'$\mid\Psi\mid}$')
+ax_2.set_ylabel(r'$\mid\Psi\mid^2$')
 
 plt.show()
