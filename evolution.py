Added separate transition computation and Simpson QF. TODO higher order Omegas hardly contribute - wrong results.

evolution.py
@@ -5,15 +5,16 @@ import matplotlib.pyplot as plt
 
 T = 10.
 tau = 0.01
-id = 'rect'
+id = 'rz'
 
 A, y0 = get_example(id)
 
-mi = MagnusIntegrator(A, order=4)
+mi = MagnusIntegrator(order=2)
 
-ts, ys = mi.evolve(y0, T, tau)
+ts, ys = mi.evolve(A, y0, T, tau)
 
 y_test = np.array([1., -1.]) / np.sqrt(2.)
+y_test = np.array([0., 1.])
 tp = np.abs(mi.t_braket(y_test, ys))
 
 fig = plt.figure(figsize=(9, 6))

magnus.py
@@ -8,22 +8,31 @@ sig_x = np.array([[0., 1.], [1., 0.]])
 sig_y = np.array([[0., -1.j], [1.j, 0.]])
 sig_z = np.array([[1., 0.], [0., -1.]])
 
-def get_example(id='std'):
+def get_example(id='std', **kwargs):
     if id == 'std':
-        A = lambda t: np.array([[1j, 2 + 1j - np.cos(2 * np.pi * t)], [-2 + 1j + np.cos(2 * np.pi * t), 3 * 1j]])
+        H = lambda t: 1.j*np.array([[1j, 2 + 1j - np.cos(2 * np.pi * t)], [-2 + 1j + np.cos(2 * np.pi * t), 3 * 1j]])
         y0 = np.array([1., 1.]) / np.sqrt(2.)
     elif id == 'rect':
-        f = lambda t: 1. if t>=0. else 0.
-        A = lambda t: -1.j*f(t)*(sig_x*np.cos(t) - sig_y*np.sin(t))
-        y0 = np.array([1., 1.]) / np.sqrt(2.)
-        #y0 = np.array([1., 0.])
+        om = kwargs.get('om', 1.)
+        V0 = kwargs.get('V0', 1.)
 
+        f = lambda t: 1. if t>=0. else 0.
+        H = lambda t: 0.5*om*sig_z + V0*f(t)*sig_x
+        y0 = np.array([0., 1.])
+
+    elif id=='rz':
+        V0 = 1./np.pi
+        T = 1.
+        V = lambda t: V0/np.cosh(t)
+        H = lambda t: V(t)*(sig_x * np.cos(t) - sig_y*np.sin(t))
+        y0 = np.array([1., 0.])
+
+    A = lambda t: -1.j*H(t)
     return A, y0
 
 
 class MagnusIntegrator:
-    def __init__(self, A, order=2, qf='midpoint'):
-        self.A = A
+    def __init__(self, order=2, qf='simpson'):
         self.order = order
         self.qf = qf
 
@@ -40,11 +49,13 @@ class MagnusIntegrator:
         Om = self.fOmega(t_e, n=1)
         for n in range(2, self.order + 1):
             Om_n = self.fOmega(t_e, n=n)
+            print('n', n, 'Om', Om_n)
             Om += Om_n
 
         return Om
 
-    def evolve(self, y0, T, tau=0.01):
+    def evolve(self, A, y0, T, tau=0.01):
+        self.A = A
         Nt = int(T / tau)
         ts = np.arange(0, Nt + 1) * tau
         if Nt * tau < T:
@@ -65,15 +76,17 @@ class MagnusIntegrator:
             return Om_1
         else:
             Om_n = self.quad(self.fOmega_p, s, n=n)
+            #Om_n = 0.
+            #for j in range(1, n):
+            #    Om_n+= self.b_n[j] / self.fac_n[j]*self.quad(self.S, s, n=n, j=j)
             return Om_n
 
     def fOmega_p(self, s, n=2):
         omp = 0.
         for j in range(1, n):
-            k_js = self.gen_ks(n, j)
             S_j = 0.
             # loop over possible variations for given j
-            for k in k_js:
+            for k in self.ks['{}_{}'.format(n, j)]:
                 # right sided, nested commutators
                 B_k = self.comm(self.fOmega(s, n=k[0]), self.A(s))
                 # loop through indices in variation
@@ -84,6 +97,19 @@ class MagnusIntegrator:
             # sum over j before integration
             omp += self.b_n[j] / self.fac_n[j] * S_j
         return omp
+
+    def S(self, s, n, j):
+        S_j = 0.
+        # loop over possible variations for given j
+        for k in self.ks['{}_{}'.format(n, j)]:
+            # right sided, nested commutators
+            B_k = self.comm(self.fOmega(s, n=k[0]), self.A(s))
+            # loop through indices in variation
+            for ik in range(1, len(k)):
+                B_k += self.comm(self.fOmega(s, n=k[ik]), B_k)
+
+            S_j += B_k
+        return S_j
 
     def gen_ks(self, n, j):
         # generate possible variations of k which sum to n - 1
@@ -102,15 +128,16 @@ class MagnusIntegrator:
         return M1 @ M2 - M2 @ M1
 
     def quad(self, f, t, **kwargs):
+        dt = t - self.t0
         if self.qf == 'midpoint':
             dt = t - self.t0
             res = f(self.t0 + dt / 2., **kwargs) * dt
 
-            return res
         elif self.qf == 'simpson':
-            #TODO to implement
+            tm = (self.t0 + t)/2.
+            res = dt/6. * (f(self.t0, **kwargs) + 4. * f(tm, **kwargs) + f(t, **kwargs))
 
-            return None
+        return res
 
     def t_braket(self, phi, psi_t):
         return np.einsum('i, ni->n', np.conj(phi), psi_t)